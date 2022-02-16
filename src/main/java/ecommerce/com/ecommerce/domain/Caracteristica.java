e23ef311af3658a749b2a006b1534baa6ae5c949
germanduci <germanducis@gmail.com>
2022-02-16 20:27:03 -0300

    Eliminando clases innecesarias

--- src/main/java/ecommerce/com/ecommerce/domain/Caracteristica.java
+++
@@ -1,9 +0,0 @@
-
-package ecommerce.com.ecommerce.domain;
-
-public class Caracteristica {
-    private String id;
-    private String titulo;
-    private String descripcion;
-    
-}
--- src/main/java/ecommerce/com/ecommerce/domain/Producto.java
+++ src/main/java/ecommerce/com/ecommerce/domain/Producto.java
@@ -1,11 +1,18 @@
-
 package ecommerce.com.ecommerce.domain;
 
 import ecommerce.com.ecommerce.enums.Envio;
+import lombok.Data;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
+@Data
+@Entity
 public class Producto {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private String id;
   private String nombre;
   private String marca;
@@ -13,8 +20,8 @@ public class Producto {
   private Integer precio;
   private Integer descuento;
   private Integer precioActual;
+  @OneToOne
   private Categoria categoria;
-  private List <Caracteristica> caracteristica;
   private Envio tipoEnvio;
   private List <Foto> fotos;
   private Date alta;
