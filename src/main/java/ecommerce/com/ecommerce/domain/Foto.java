a2d9ab50b2178dffe23a1d7a30b73822637a1e0a
germanduci <germanducis@gmail.com>
2022-02-19 11:44:22 -0300

    Cambios generales

--- src/main/java/ecommerce/com/ecommerce/domain/Foto.java
+++ src/main/java/ecommerce/com/ecommerce/domain/Foto.java
@@ -1,14 +1,18 @@
 
 package ecommerce.com.ecommerce.domain;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Data
 @Entity
+@Table(name="foto")
 public class Foto implements Serializable{
     private static final long serialVersionUID = 1L;
     @Id
-    //wTF?
     private String id;
 }
