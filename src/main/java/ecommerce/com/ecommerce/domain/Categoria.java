f3a7021ad74e3875e75da641252499d5dd3ee4b2
germanduci <germanducis@gmail.com>
2022-02-19 11:27:11 -0300

    Cambios generales

--- src/main/java/ecommerce/com/ecommerce/domain/Categoria.java
+++ src/main/java/ecommerce/com/ecommerce/domain/Categoria.java
@@ -8,9 +8,9 @@ import javax.persistence.*;
 @Entity
 @Table(name="categoria")
 public class Categoria implements Serializable{
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String titulo;
-
 }
--- src/main/java/ecommerce/com/ecommerce/domain/Transaccion.java
+++ src/main/java/ecommerce/com/ecommerce/domain/Transaccion.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Entity
 @Table(name="transaccion")
 public class Transaccion implements Serializable{
-    
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
